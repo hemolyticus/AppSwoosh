52ab9c394849bd8092e34d43a17898b3781322a1
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-08-30 11:08:14 +1200

    programatic segue to SkillVCSegue

--- Controller/LeagueVC.swift
+++ Controller/LeagueVC.swift
@@ -16,20 +16,13 @@ class LeagueVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    
+    @IBAction func onNextTapped(_ sender: UIButton) {
+        //This shows a VC
+        performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
